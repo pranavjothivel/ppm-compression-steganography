fe3b9ba465e02dd9987a6f0e02e057426cdb46d7
Pranav Jothivel <44120295+pranavjothivel@users.noreply.github.com>
2024-10-23 22:42:02 +0000

    define QTNode recursive struct and add helper functions for create_quadtree

--- include/qtree.h
+++ include/qtree.h
@@ -3,14 +3,24 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #include "image.h"
 
 #define INFO(...) do {fprintf(stderr, "[          ] [ INFO ] "); fprintf(stderr, __VA_ARGS__); fprintf(stderr, "\n"); fflush(stderr);} while(0)
 #define ERROR(...) do {fprintf(stderr, "[          ] [ ERR  ] "); fprintf(stderr, __VA_ARGS__); fprintf(stderr, "\n"); fflush(stderr);} while(0) 
 
+// QTNode children
+// 1 2
+// 3 4
 typedef struct QTNode {
-    char placeholder[1];  // This is a placeholder. You should not use this struct.    
+    unsigned char intensity;
+    int row, col; // starting top-left position
+    int width, height; // <row + width, col + height> is the ending bottom-right position
+    struct QTNode *child1;
+    struct QTNode *child2;
+    struct QTNode *child3;
+    struct QTNode *child4;
 } QTNode;
 
 QTNode *create_quadtree(Image *image, double max_rmse);  
@@ -23,5 +33,9 @@ void delete_quadtree(QTNode *root);
 void save_qtree_as_ppm(QTNode *root, char *filename); 
 QTNode *load_preorder_qt(char *filename);
 void save_preorder_qt(QTNode *root, char *filename);  
+
+// my functions
+unsigned char compute_rmse(Image *image, int row, int width, int col, int height);
+double compute_average_intensity(Image *image, int row, int width, int col, int height);
 
 #endif // QTREE_H
